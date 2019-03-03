10aa51dd988c4af6851c376c1de5e7e783493f26
TTacco <joventadem@gmail.com>
2019-03-03 16:50:01 +0800

    weary

--- Board.java
+++ Board.java
@@ -1,41 +1,39 @@
 import jdk.nashorn.internal.runtime.regexp.joni.exception.SyntaxException;
 
+import static java.lang.System.out;
+
 public class Board {
     char board[][] =
             {
-                    {'W','W','W','o'},
-                    {'W','W','o','B'},
-                    {'W','o','B','B'},
-                    {'o','B','B','B'},
+                    {'W', 'W', 'W', 'o'},
+                    {'W', 'W', 'o', 'B'},
+                    {'W', 'o', 'B', 'B'},
+                    {'o', 'B', 'B', 'B'},
             };
 
 
-
-    public int MovePiece(char player, int srcI, int srcJ, int dstI, int dstJ){
+    public int MovePiece(char player, int srcI, int srcJ, int dstI, int dstJ) {
         boolean validMove = true;
 
 
         validMove = ValidMoveCheck(player, srcI, srcJ, dstI, dstJ);
 
 
-        if(validMove){
+        if (validMove) {
             //Swap Characters Here
 
             return 1;
-        }
-        else{
+        } else {
             return -1;
         }
     }
 
-
-    public boolean ValidMoveCheck(char player, int srcI, int srcJ, int dstI, int dstJ){
-        try
-        {
+    //Source I, Source J, Destination I, Destination J
+    public boolean ValidMoveCheck(char player, int srcI, int srcJ, int dstI, int dstJ) {
+        try {
             TestCaseTwo(player, srcI, srcJ);
-            TestCaseThree(player);
-        }
-        catch(Exception e){
+            TestCaseFour(TestCaseThree(player), srcI, srcJ, dstI, dstJ);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return false;
         }
@@ -74,16 +72,84 @@ public class Board {
 
     int ArrayMove(int z, char p) {
 
-        if(p == 'W'){
-           return z+1;
-        }
-        else if (p == 'B'){
-           return z-1;
+        if (p == 'W') {
+            return z + 1;
+        } else if (p == 'B') {
+            return z - 1;
         }
 
         return 0;
     }
 
+    public void TestCaseFour(boolean canSwapOrPass, int srcI, int srcJ, int dstI, int dstJ) {
+        /*
+        North, South, East, West movements
+        Outer if checks if the player is trying to move horizontally
+        Inner If the distance is greater than 1 movement we know that the player is trying to hop, else simple movement
+        */
 
+        if (srcI == dstI && srcJ == dstJ) {
+            if (canSwapOrPass) {
+                out.println("Player has Passed");
+            } else {
+                throw new SyntaxException("You pass turns yet as you still have forward moves");
+            }
+        } else if (srcI == dstI || srcJ == dstJ) {
+            //out.println("Movement detected");
+            //Hop Movement Horizontal
 
+            if (Math.abs(srcJ - dstJ) > 1) {
+                //out.println("HOP CHECK H");
+
+                if (!(board[dstI][dstJ] == 'o')) {
+                    throw new SyntaxException("Cannot hop to an existing piece");
+                }
+
+                for (int b = srcJ; b != dstJ; b = Increment(b, srcI, srcJ, dstI, dstJ)) {
+                    if (board[srcI][b] == 'o') {
+                        throw new SyntaxException("Cannot hop horizontally");
+                    }
+
+                }
+
+            }
+            //Hop Movement Vertical
+            else if (Math.abs(srcI - dstJ) > 1) {
+                //out.println("HOP CHECK V");
+
+                if (!(board[dstI][dstJ] == 'o')) {
+                    throw new SyntaxException("Cannot hop to an existing piece");
+                }
+
+                for (int a = srcI; a != srcI; a = Increment(a, srcI, srcJ, dstI, dstJ)) {
+                    if (board[a][srcJ] == 'o') {
+                        throw new SyntaxException("Cannot hop vertically");
+                    }
+                }
+
+            }
+            //Single Movement
+            else if (Math.abs(srcI - dstI) == 1 || Math.abs(srcJ - dstJ) == 1) {
+                out.println("Moved only one step");
+                if ((board[dstI][dstJ] == 'W'
+                        || board[dstI][dstJ] == 'B')
+                        && !canSwapOrPass) {
+                    throw new SyntaxException("There is a piece that already exists in that area and the player is not allowed to swap yet");
+                }
+            }
+        } else {
+            throw new SyntaxException("Cannot move vertically");
+        }
+
+    }
+
+    //Handles incrementation for TestCaseFour
+    public int Increment(int z, int srcI, int srcJ, int dstI, int dstJ) {
+
+        if (srcI < dstI || srcJ < dstJ) {
+            return ++z;
+        } else {
+            return --z;
+        }
+    }
 }
